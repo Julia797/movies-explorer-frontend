f803712b449508eeae0718a0b9578de3112bfc47
Julia797 <almatezza@inbox.ru>
2023-10-16 06:17:47 +0300

    добавление фильмов по кнопке еще

--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -2,21 +2,67 @@ import './MoviesCardList.css';
 import MoviesCard from '../MoviesCard/MoviesCard';
 import Preloader from '../Preloader/Preloader'
 import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 function MoviesCardList({ movies, saveMovies, handleMovieLike, handleDeleteMovie, isLoadingMovies, errorNoMovie }) {
   const { pathname } = useLocation()
-  
-  function handleMore() {
-    
-  }
+  const [visibleMovies, setVisibleMovies] = useState([]);
+  const [showMoreButton, setShowMoreButton] = useState(false);
 
+  const getCardsCount = () => {
+    const screenWidth = window.innerWidth;
+    if (screenWidth >= 1280) {
+      return 16;
+    } else if (screenWidth >= 1168) {
+      return 12;
+    } else if (screenWidth >= 768) {
+      return 8;
+    } else if (screenWidth >= 749) {
+      return 4;
+    } else {
+      return 5;
+    }
+  };
+ 
+  const сount = () => {
+    const screenWidth = window.innerWidth;
+    if (screenWidth >= 1280) {
+      return 4;
+    } else if (screenWidth >= 1170) {
+      return 3;
+    } else if (screenWidth >= 768) {
+      return 2;
+    } else if (screenWidth >= 750) {
+      return 1;
+    } else {
+      return 2;
+    }
+  };
+ 
+  useEffect(() => {
+    setVisibleMovies(movies.slice(0, getCardsCount()));
+    setShowMoreButton(movies.length > getCardsCount());
+  }, [movies]);
+
+  const handleShowMore = () => {
+    setVisibleMovies(prevVisibleMovies => [
+      ...prevVisibleMovies,
+      ...movies.slice(prevVisibleMovies.length, prevVisibleMovies.length + сount())
+    ]);
+
+    if (visibleMovies.length + getCardsCount() >= movies.length) {
+      setShowMoreButton(false);
+    }
+  };
+  
+ 
   return (
     <>
       <section className="elements" aria-label="видеофильм">
         <ul className="element">
           {isLoadingMovies ? <Preloader /> :
             (pathname === '/movies') ?
-              movies.map(data => {
+            visibleMovies.map(data => {
                 return (
                   <MoviesCard 
                     data={data}
@@ -43,7 +89,7 @@ function MoviesCardList({ movies, saveMovies, handleMovieLike, handleDeleteMovie
                     <span className='element__error'>Ничего не найдено</span>
           } 
         </ul>
-        {pathname === '/movies' && <button className="elements__more" type="button" name="more" onClick={handleMore}>Ещё</button>}
+        {showMoreButton && pathname === '/movies' && <button className="elements__more" type="button" name="more" onClick={handleShowMore}>Ещё</button>}
       </section>
     </>
   )
